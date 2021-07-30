9806252bb16353178a500511c63754cdf0c4770c
Stepami <Stepami@mail.ru>
2021-07-30 16:10:36 +0300

    добавил корсы и метод оценки качества кластеризации

--- text_api.py
+++ text_api.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
+from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF, PCA
 from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 from text_storage import read_from_json, fetch_courses_from_db
 from text_transform import process_text
 
@@ -17,6 +19,8 @@ api = Api(
     description='Text NLP processing API'
 )
 
+CORS(app)
+
 # https://sanjayasubedi.com.np/nlp/nlp-with-python-topic-modeling/
 # example /api/topics/20/15
 @api.route('/api/topics/<int:topics_num>/<int:top_words_num>', methods=['GET'])
@@ -26,12 +30,14 @@ api = Api(
 })
 class Topics(Resource):
     def get(self, topics_num, top_words_num):
-        processed_text = list(map(lambda obj: obj['description'], read_from_json()))
+        processed_text = list(
+            map(lambda obj: obj['description'], read_from_json()))
 
         vectorizer = TfidfVectorizer()
         features = vectorizer.fit_transform(processed_text)
 
-        nmf = NMF(n_components=topics_num, init='random', random_state=0, max_iter=1000)
+        nmf = NMF(n_components=topics_num, init='random',
+                  random_state=0, max_iter=1000)
         nmf.fit(features)
 
         # list of unique words found by the vectorizer
@@ -54,7 +60,9 @@ class Topics(Resource):
         return jsonify(topics)
 
 parser = api.parser()
-parser.add_argument('text', type=str, location='json', help='Text to find neighbors of')
+parser.add_argument('text', type=str, location='json',
+                    help='Text to find neighbors of')
+
 @api.route('/api/neighbors/<int:neighbors_num>', methods=['POST'])
 @api.doc(params={
     'neighbors_num': 'Number of neighbors to find',
@@ -66,7 +74,8 @@ class Neighbors(Resource):
         data = read_from_json()
 
         vectorizer = TfidfVectorizer()
-        features = vectorizer.fit_transform(list(map(lambda obj: obj['description'], data)))
+        features = vectorizer.fit_transform(
+            list(map(lambda obj: obj['description'], data)))
 
         knn = NearestNeighbors(n_neighbors=neighbors_num, metric='cosine')
         knn.fit(features)
@@ -91,7 +100,8 @@ class Clusters(Resource):
         data = read_from_json()
 
         vectorizer = TfidfVectorizer()
-        features = vectorizer.fit_transform(list(map(lambda obj: obj['description'], data)))
+        features = vectorizer.fit_transform(
+            list(map(lambda obj: obj['description'], data)))
 
         kmeans = KMeans(n_clusters=clusters_num, random_state=0)
         kmeans.fit(features)
@@ -103,13 +113,37 @@ class Clusters(Resource):
         courses = fetch_courses_from_db()
         for label, course, location in zip(kmeans.labels_, courses, reduced_features.tolist()):
             res.append({
-                'x': location[0],
-                'y': location[1],
+                'x': location[0] * 10,
+                'y': location[1] * 10,
                 'cluster': int(label),
-                'data': course
+                'data': {
+                    'title': course['title'],
+                    'url': course['url']
+                }
             })
 
         return jsonify(res)
+
+@api.route('/api/clusters/scores', methods=['POST'])
+class ClustersScores(Resource):
+    @api.expect(api.model('NumbersToCheck', {
+        'numbers': fields.List(fields.Integer)
+    }))
+    def post(self):
+        data = read_from_json()
+        numbers = request.get_json()['numbers']
+
+        vectorizer = TfidfVectorizer()
+        features = vectorizer.fit_transform(
+            list(map(lambda obj: obj['description'], data)))
+
+        scores = {}
+        for k in numbers:
+            kmeans = KMeans(n_clusters=k).fit(features)
+            labels = kmeans.labels_
+            scores[k] = silhouette_score(features, labels, metric='euclidean')
+
+        return jsonify(scores)
 
 def main():
     app.run(debug=True, port=3000)
